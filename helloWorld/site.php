c54915d2953159bd98a55f157a13e97405214c58
ergunDevv <ergncyln2127088@gmail.com>
2023-05-09 10:11:11 +0300

    Hello World added with explanations of php logic

---
+++ helloWorld/site.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <!-- For writing php in html. php tag is '<?php ?>'  -->
+    <?php
+    // Echo tag converts whatever is in the string to html
+    // so with echo tag we can write like exactly html
+    echo "<h1>Hello World</h1>";
+
+
+
+    ?>
+
+</body>
+
+</html>
