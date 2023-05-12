af486e3f194fbb50330b1dc7cb7b4d9f0a66b9f6
ergunDevv <ergncyln2127088@gmail.com>
2023-05-12 10:11:11 +0300

    Basic math functions added with explanations of php logic

---
+++ variables/math.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- In this class basic math functions will shown -->
+
+    <?php
+    // In php math calculations easy. Directly write whatever you want to do and php will calculate
+    echo 50 - 10;
+    echo '<hr/>';
+    echo 60 + 10;
+    echo '<hr/>';
+    echo 50 % 3;
+    echo '<hr/>';
+    // Math calculations work with variables too.
+    $integerValue = 12;
+    echo $integerValue - 2;
+
+    echo '<hr/>';
+    // Built-in min-max functions determines the min or max value between parameters than returns the value
+    echo min(50, 100);
+    echo '<hr/>';
+    echo max(50, 100);
+
+    echo '<hr/>';
+    // Built-in round function rounds the number closest integer number and return the value.
+    echo round(5.7)
+
+
+        ?>
+
+
+
+
+</body>
+
+</html>
